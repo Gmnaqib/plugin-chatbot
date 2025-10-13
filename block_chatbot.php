21bb81e1c3bbbd2a4205aa28e836f1983505a25d
Gmnaqib <gumilarmn16@gmail.com>
2025-10-13 15:04:42 +0700

    Refactor block_chatbot class for improved code style; remove unused chat.js file and optimize CSS transitions

--- block_chatbot.php
+++ block_chatbot.php
@@ -1,8 +1,10 @@
 <?php
-class block_chatbot extends block_base {
-    public function init() {
-        $this->title = get_string('pluginname', 'block_chatbot');
-    }
+class block_chatbot extends block_base
+{
+  public function init()
+  {
+    $this->title = get_string('pluginname', 'block_chatbot');
+  }
   public function get_content()
   {
     global $PAGE, $OUTPUT;
@@ -15,15 +17,12 @@ class block_chatbot extends block_base {
     // Include the custom CSS file
     $PAGE->requires->css('/blocks/chatbot/styles.css');
     $PAGE->requires->js('/blocks/chatbot/script.js');
-    $PAGE->requires->js('/blocks/chatbot/chat.js');
 
 
     $this->content = new stdClass;
-    $course_id= $this->page->course->id;
+    $course_id = $this->page->course->id;
     $data = ['title' => 'Welcome to the Moodle Chatbot!', 'course_id' => $course_id];
     $this->content->text = $OUTPUT->render_from_template('block_chatbot/block_chatbot', $data);
     return $this->content;
   }
-
-
 }
