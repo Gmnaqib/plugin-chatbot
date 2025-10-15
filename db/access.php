085eb9551b2ef0f108a70425eadd4026bad22d31
Gmnaqib <gumilarmn16@gmail.com>
2025-10-15 15:48:45 +0700

    Add capability for users to add chatbot block to Dashboard/My Home

--- db/access.php
+++ db/access.php
@@ -8,4 +8,14 @@ $capabilities = array(
       'manager' => CAP_ALLOW
     ),
   ),
+
+  // *** TAMBAHAN PENTING UNTUK DUKUNGAN MOBILE/DASHBOARD ***
+  'block/chatbot:myaddinstance' => array(
+    'captype' => 'write',
+    'contextlevel' => CONTEXT_SYSTEM,
+    'archetypes' => array(
+      'user' => CAP_ALLOW // Izinkan pengguna untuk menambahkan ke Dashboard/My Home
+    ),
+    'clonepermissionsfrom' => 'moodle/my:manageblocks'
+  ),
 );
