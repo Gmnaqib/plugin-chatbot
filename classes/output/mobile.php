b2e4dc23d2009ae9e2d9dbe85a2ed5b82d0883d0
Gmnaqib <gumilarmn16@gmail.com>
2025-10-15 23:14:51 +0700

    Refactor mobile class to avoid conflicts with external API

--- classes/output/mobile.php
+++ classes/output/mobile.php
@@ -4,41 +4,44 @@ namespace block_chatbot\output;
 
 defined('MOODLE_INTERNAL') || die();
 
-use external_api;
+// Hapus baris use external_api; untuk menghindari konflik.
 use core_external\external_function_parameters;
-use core_external\external_value;
+// Hapus baris use external_value; untuk menghindari konflik.
 use context_block;
 use context_course;
 use context_system;
 use block_chatbot\block_chatbot;
 
+// MENGGUNAKAN FULL NAMESPACE UNTUK MENGHINDARI KONFLIK
 class mobile extends \core_external\external_api
 {
     /**
      * Mengembalikan parameter eksternal untuk mobile_view.
-     * Harus mendeklarasikan parameter courseid yang dikirim oleh Moodle App.
+     * Menggunakan \external_value dan konstanta global.
      *
      * @return external_function_parameters
      */
     public static function mobile_view_parameters()
     {
         return new external_function_parameters([
-            // Wajib mendefinisikan courseid (ID kursus), 0 untuk konteks sistem/My Home
-            new external_value(external_value::TYPE_INT, 'The course id for the context.', VALUE_DEFAULT, 0)
+            // Menggunakan backslash (\) untuk mengakses konstanta global
+            new \external_value(\external_value::TYPE_INT, 'The course id for the context.', \VALUE_DEFAULT, 0)
         ]);
     }
 
     /**
      * Mengembalikan struktur output untuk mobile_view
+     * Menggunakan \external_value dan konstanta global.
      *
-     * @return external_value
+     * @return \external_value
      */
     public static function mobile_view_returns()
     {
-        return new external_value(external_value::TYPE_OBJECT, 'Block content for mobile', [
-            'templates' => new external_value(external_value::TYPE_ARRAY, 'Block templates', false, null, new external_value(external_value::TYPE_OBJECT)),
-            'javascript' => new external_value(external_value::TYPE_STRING, 'Javascript to run', false, ''),
-            'otherdata' => new external_value(external_value::TYPE_OBJECT, 'Other data', false, null)
+        // Menggunakan backslash (\) untuk mengakses konstanta global
+        return new \external_value(\external_value::TYPE_OBJECT, 'Block content for mobile', [
+            'templates' => new \external_value(\external_value::TYPE_ARRAY, 'Block templates', false, null, new \external_value(\external_value::TYPE_OBJECT)),
+            'javascript' => new \external_value(\external_value::TYPE_STRING, 'Javascript to run', false, ''),
+            'otherdata' => new \external_value(\external_value::TYPE_OBJECT, 'Other data', false, null)
         ]);
     }
 
@@ -57,10 +60,10 @@ class mobile extends \core_external\external_api
 
         // Tentukan konteks: kursus atau sistem
         if ($courseid > 0) {
-            $context = context_course::instance($courseid, MUST_EXIST);
+            $context = \context_course::instance($courseid, \MUST_EXIST);
         } else {
             // Untuk My home/Dasbor (Sistem)
-            $context = context_system::instance();
+            $context = \context_system::instance();
         }
 
         // Pemeriksaan izin: pengguna harus bisa melihat blok di konteks ini.
--- version.php
+++ version.php
@@ -1,4 +1,4 @@
 <?php
 $plugin->component = 'block_chatbot';
-$plugin->version = 2025010714;
+$plugin->version = 2025010715;
 $plugin->requires = 2022112800;
